b137106d2a752e55ddfb505670617f97e69f7a2f
Mahdiyar <mahdiyar.barzegar@gmail.com>
2025-12-05 19:10:01 +0330

    refactor(rca): add status flags to the ripple-carry-adder module

--- src/ripple-carry-adder/hdl/ripple_carry_adder.v
+++ src/ripple-carry-adder/hdl/ripple_carry_adder.v
@@ -2,15 +2,23 @@ module ripple_carry_adder #(
   parameter BUS_WIDTH = 32
 ) (
   input                  add_sub_b,
+  input                  sign,
   input  [BUS_WIDTH-1:0] in1,
   input  [BUS_WIDTH-1:0] in2,
   output [BUS_WIDTH-1:0] out,
-  output                 ovf
+  output                 z,
+  output                 n,
+  output                 c,
+  output                 v
 );
 
   wire [BUS_WIDTH:0] carry;
   assign carry[0] = add_sub_b;
-  assign ovf      = carry[BUS_WIDTH] ^ carry[BUS_WIDTH-1];
+
+  assign z        = ~(|out);
+  assign n        = (sign) ? out[BUS_WIDTH-1] : 1'b0;
+  assign c        = carry[BUS_WIDTH];
+  assign v        = (sign) ? carry[BUS_WIDTH] ^ carry[BUS_WIDTH-1] : 1'b0;
 
   genvar i;
   generate
--- src/ripple-carry-adder/sim/gui_ripple_carry_adder.v
+++ src/ripple-carry-adder/sim/gui_ripple_carry_adder.v
@@ -5,28 +5,35 @@ module gui_ripple_carry_adder;
   parameter BUS_WIDTH = 32;
 
   reg                  add_sub_b;
+  reg                  sign;
   reg  [BUS_WIDTH-1:0] a;
   reg  [BUS_WIDTH-1:0] b;
   wire [BUS_WIDTH-1:0] y;
-  wire                 ovf;
+  wire z, n, c, v;
 
   ripple_carry_adder #(
     .BUS_WIDTH(BUS_WIDTH)
   ) rca (
     .add_sub_b(add_sub_b),
+    .sign     (sign),
     .in1      (a),
     .in2      (b),
     .out      (y),
-    .ovf      (ovf)
+    .z        (z),
+    .n        (n),
+    .c        (c),
+    .v        (v)
   );
 
   initial begin
     add_sub_b = 'b1;
+    sign      = 'b0;
     a         = 'd110;
     b         = 'd24;
     #10;
 
     add_sub_b = 'b1;
+    sign      = 'b0;
     a         = 'd24;
     b         = 'd110;
     #10;
